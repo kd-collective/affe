6e06b8efcd5b57160ba267e42cbf3b982a4b89a1
Daniel Mendler <mail@daniel-mendler.de>
2026-03-22 01:19:43 +0100

    Update header

--- affe-backend.el
+++ affe-backend.el
@@ -2,8 +2,6 @@
 
 ;; Copyright (C) 2021-2026 Daniel Mendler
 
-;; This file is not part of GNU Emacs.
-
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
